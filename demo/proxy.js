199b450492d0525d498b7ce7776d29d2449527a4
josh <postor@gmail.com>
2018-08-13 17:52:54 +0800

    通过代理下载

--- demo/proxy.js
+++ demo/proxy.js
@@ -1,20 +1,6 @@
-const Proxies = require('free-proxies').default;
+const Proxies = require('free-http-proxy');
 const proxies = new Proxies();
-let currentProxy, currentProxyList, currentIndex = 0;
 
 module.exports = async function getProxy(forceNew = false) {
-  if (forceNew || !currentProxy) {
-    if (!currentProxyList || currentIndex == currentProxyList.length - 1) {
-      currentProxyList = await proxies.getUSProxie()
-      currentIndex = 0
-      currentProxy = currentProxyList[currentIndex]
-      return currentProxy
-    }
-
-    currentIndex += 1
-    currentProxy = currentProxyList[currentIndex]
-    return currentProxy
-  }
-  return currentProxy
-
+  return proxies.getProxy(forceNew)
 }
