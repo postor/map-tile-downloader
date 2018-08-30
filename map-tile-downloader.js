ed3992ae2e2294da7c07f751d8993629284cdfbf
josh <postor@gmail.com>
2018-08-30 11:46:07 +0800

    增加跳过提示

--- map-tile-downloader.js
+++ map-tile-downloader.js
@@ -57,8 +57,10 @@ module.exports = {
             //create writestream as z/x/y.png
             var ws = (xPath + '/' + tileCoords.y + (options.extension||'.png'));
             if(!fs.existsSync(ws)){
-                //fs.appendFileSync('to-download.csv',`${url},${ws}\n`);   
+                //fs.appendFileSync('to-download.csv',`${url},${ws}\n`);                   
                 await download(url,ws)         
+            }else{
+                console.log(`file ${ws} exists, skip`)
             }
             
             //request(url).pipe(ws);
